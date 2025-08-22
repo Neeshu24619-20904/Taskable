105625bc55575930143b65983a23ea9db5d81522
neeshu <neeshant.24bcs10635@sst.scaler.com>
2025-08-22 14:05:56 +0530

    origin

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -7,6 +7,18 @@ router.get("/", async (req, res) => {
   const tasks = await Task.find().sort({ order: 1 });
   res.json(tasks);
 });
+ // fetching a single task 
+router.get("/:id" , async(req , res ) =>{
+
+   try {
+    const task = await Task.findById(req.params.id) ;
+    if (!task ) return res.status(404).json ({msg: "Task not found "})
+      res.json(task)
+   }
+   catch(err){
+    res.status(500).json({error: err.message})
+   }
+});
 
 // POST new task
 router.post("/", async (req, res) => {
@@ -15,5 +27,24 @@ router.post("/", async (req, res) => {
   await newTask.save();
   res.json(newTask);
 });
+
+// this si to update the task 
+// router.put("/:id" , async(req ,res) => {
+//   try{
+//     const {title , duration , order } = req.body ;
+//     const updateTask =await Task.findbyIdAndUpdate(
+//       req.params.id,
+//       {title , duration , order } ,
+//       {new:true}
+//       if(!updatedTask) return res.sattus 
+//     )`
+
+//   }
+//   catch {
+
+//   }
+// });
+
+
 
 module.exports = router;
